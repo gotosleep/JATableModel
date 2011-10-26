6e23ba6b158f43026dd4f2a8189b0bed30d648b9
Jesse Andersen <jesse@scribd.com>
2011-10-26 16:10:48 -0700

    reorganizing project

---
+++ Example/BasicExample/BasicExample/JAAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  JAAppDelegate.h
+//  BasicExample
+//
+//  Created by Jesse Andersen on 10/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface JAAppDelegate : UIResponder <UIApplicationDelegate> {
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@property (nonatomic, retain) UIViewController *viewController;
+
+@end
---
+++ Example/BasicExample/BasicExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BasicExample
+//
+//  Created by Jesse Andersen on 10/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JAAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([JAAppDelegate class]));
+	}
+}
